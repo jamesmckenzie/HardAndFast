d67a2a0cdd0364656931d10273a884598455e4fb
simonmarksuk <simon.marks1987@gmail.com>
2015-05-19 17:21:15 +0100

    Log and store measurements

--- Patient/Patient.js
+++ Patient/Patient.js
@@ -1,34 +1,54 @@
-
 UserMeasurements = new Meteor.Collection("userMeasurements");
 
-
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to Patient."};
+	Template.hello.greeting = function () {
+		return "Welcome to Patient.";
+	};
 
-  Template.measurements.users = function () {
-    return Users.find({}, {sort: {lastName: 1}})};
+	Template.measurements.users = function () {
+		return Users.find({}, {sort: {lastName: 1}});
+	};
 
+	Template.measurements.helpers({
+		fields: [
+			{ name: "bloodPressure", label: "Blood Pressure", value: 0 },
+			{ name: "heartRate", label: "Heart Rate", value: 0 },
+			{ name: "weight", label: "Weight", value: 0 }
+		],
+		
+		measurements: function () {
+			return UserMeasurements.find({}, {sort: {date: -1}});
+		}
+	});
+	
+	Template.measurements.events({
+		'submit .submit-measurements': function(event) {
+			UserMeasurements.insert({
+				date: new Date(),
+				bloodPressure: event.target.bloodPressure.value,
+				heartRate: event.target.heartRate.value,
+				weight: event.target.weight.value
+			});
+			
+			event.target.bloodPressure.value = '0';
+			event.target.heartRate.value = '0';
+			event.target.weight.value = '0';
+			
+			return false;
+		}
+	});
 
-  Template.measurements.helpers({
-	types: [
-		{name : "Blood Pressure", value: 0},
-		{name : "Heart Rate", value: 0},
-		{name : "Weight", value: 0}
-	]
-  });
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+	Template.hello.events({
+		'click input' : function () {
+		// template data, if any, is available in 'this'
+		if (typeof console !== 'undefined')
+			console.log("You pressed the button");
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+	});
 }
