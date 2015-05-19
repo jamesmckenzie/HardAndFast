406c4c53663084125c6171172ce2229dba3524cf
TobyBlackmore <tobyb1@hotmail.com>
2015-05-19 14:39:29 +0100

    Added test users and measurements

--- Patient/Patient.js
+++ Patient/Patient.js
@@ -1,7 +1,28 @@
+
+UserMeasurements = new Meteor.Collection("userMeasurements");
+Users = new Meteor.Collection("users");
+
+Users.remove({});
+Users.insert({firstName: "Bob", lastName: "Watercress"});
+Users.insert({firstName: "Ryan", lastName: "Chipmunk"});
+Users.insert({firstName: "Rachel", lastName: "Rubens"});
+
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to Patient.";
-  };
+    return "Welcome to Patient."};
+
+  Template.measurements.users = function () {
+    return Users.find({}, {sort: {lastName: 1}})};
+
+
+  Template.measurements.helpers({
+	types: [
+		{name : "Blood Pressure", value: 0},
+		{name : "Heart Rate", value: 0},
+		{name : "Weight", value: 0}
+	]
+  });
 
   Template.hello.events({
     'click input' : function () {
