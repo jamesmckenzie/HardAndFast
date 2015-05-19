5ddcdba21de14fe7c28885ac9170a868d6e90f7b
TobyBlackmore <tobyb1@hotmail.com>
2015-05-19 17:00:00 +0100

    Cleanup patient and only show measurements if logged in

--- Patient/Patient.js
+++ Patient/Patient.js
@@ -3,8 +3,6 @@ UserMeasurements = new Meteor.Collection("userMeasurements");
 
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to Patient."};
 
   Template.measurements.users = function () {
     return Users.find({}, {sort: {lastName: 1}})};
@@ -16,14 +14,6 @@ if (Meteor.isClient) {
 		{name : "Heart Rate", value: 0},
 		{name : "Weight", value: 0}
 	]
-  });
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
   });
 }
 
